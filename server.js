80295a8b42fbf74a35f52105d2e418d88dc70ed5
ndagaby111-sketch <ndagaby111@gmail.com>
2026-02-02 21:15:38 +0530

    first upload

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+
+// middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public")); // serve HTML files
+
+// contact form route
+app.post("/contact", (req, res) => {
+  const { name, email, message } = req.body;
+
+  console.log("New Message:");
+  console.log(name, email, message);
+
+  res.send("Message received successfully!");
+});
+
+// start server
+app.listen(3000, () => {
+  console.log("Server running at https://gabriel-portfolio.onrender.com");
+});
